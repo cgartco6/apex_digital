2a11d3f37302c767caf8a90f58444ab91273855a
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-07-17 12:42:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from agents.support_agent import SupportAgent, app as support_app
 from agents.growth_agent import GrowthAgent
 from agents.devsec_agent import DevSecAgent
 import threading
+from utils.security_utils import *
+from utils.payment_utils import *
+from utils.media_utils import *
+from utils.agent_utils import *
+from utils.system_utils import *
+from utils.sa_utils import *
 
 def run_support_agent():
     support_app.run(port=5000)
